c44fdea59370f6dd30188631e2119243340b12c1
Theenadayalan K <puduvai.theena@gmail.com>
2018-11-20 12:06:50 +0530

    Exit process on failure of executing git diff command

--- utils/fetchGitDiff.js
+++ utils/fetchGitDiff.js
@@ -1,26 +1,18 @@
-const chalk = require("chalk");
-const warning = chalk.keyword("orange");
-const { log } = console;
 const { spawnChildProcess } = require("./common");
 
 module.exports = function fetchGitDiff( baseBranch = "master" ) {
-  // git command to pull out the changed file names between current branch and master (Exclude delelted files which cannot be fetched now)
+  // git command to pull out the changed file names between current branch and master (Excluded delelted files which cannot be fetched now)
   let command = `git diff --relative --name-only --diff-filter=d ${baseBranch}...HEAD`;
 
-  return new Promise(resolve => {
+  return new Promise( (resolve, reject) => {
     spawnChildProcess({ command }, ({ hasErrors = false, output = {} }) => {
       let fileList = [];
       if (hasErrors) {
-        log(
-          warning(
-            `Fetching commited files process closed with errors: \n ${output.stderr}`
-          )
-        );
-      } else {
-        output.stdout.split("\n").forEach(filename => {
-          filename ? fileList.push(filename) : null;
-        });
+        reject(`\n Fetching committed file list process has been stopped with the following error: \n ${output.stderr}`);
       }
+      output.stdout.split("\n").forEach(filename => {
+        filename ? fileList.push(filename) : null;
+      });
       resolve(fileList);
     });
   });
