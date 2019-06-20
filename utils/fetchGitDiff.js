6cb5e343b0c88e05eb23fbfe48130e6c6166bb8d
Eric Allen <eric@ericrallen.dev>
2019-06-20 19:38:26 -0400

    feat: use currentBranch for diff if it has already been pushed to remote

--- utils/fetchGitDiff.js
+++ utils/fetchGitDiff.js
@@ -3,7 +3,7 @@ const { execChildProcess } = require("./common");
 module.exports = function fetchGitDiff( baseBranch = "master" ) {
 
   // git command to pull out the changed file names between current branch and base branch (Excluded delelted files which cannot be fetched now)
-  let command = `git diff --relative --name-only --diff-filter=d ${baseBranch}...HEAD`;
+  let command = `git diff --relative --name-only --diff-filter=d ${baseBranch}`;
 
   return new Promise( (resolve, reject) => {
     return execChildProcess({ command })
