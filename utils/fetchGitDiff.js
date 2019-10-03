f5150751f421070ecccedac10b33211e204de8e7
Theenadayalan K <puduvai.theena@gmail.com>
2019-10-03 20:44:15 +0530

    Reverting back to old git diff syntax

--- utils/fetchGitDiff.js
+++ utils/fetchGitDiff.js
@@ -3,7 +3,7 @@ const { execChildProcess } = require("./common");
 module.exports = function fetchGitDiff( baseBranch = "master" ) {
 
   // git command to pull out the changed file names between current branch and base branch (Excluded delelted files which cannot be fetched now)
-  let command = `git diff --relative --name-only --diff-filter=d ${baseBranch}`;
+  let command = `git diff --relative --name-only --diff-filter=d ${baseBranch}...HEAD`;
 
   return new Promise( (resolve, reject) => {
     return execChildProcess({ command })
